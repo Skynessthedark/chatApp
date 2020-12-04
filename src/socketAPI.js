f458aa09a0c80d16bfa496f6a2a7415e00fcaee7
Skynessthedark <qksnn.9@gmail.com>
2020-12-05 01:06:32 +0300

    Socket Authorization

--- src/socketAPI.js
+++ src/socketAPI.js
@@ -1,12 +1,25 @@
 const socketio = require('socket.io');
+const socketAuthorization = require('../middleware/socketAuthorization');
 const io = socketio();
 
 const socketApi = {
     io
 };
 
+//Socket Authorization
+io.use(socketAuthorization);
+
+/**
+ * REDIS ADAPTER
+ */
+const redisAdapter = require('socket.io-redis');
+io.adapter(redisAdapter({
+    host: process.env.REDIS_URI,
+    port: process.env.REDIS_PORT
+}));
+
 io.on('connection', socket =>{
-    console.log('a user logged in.');
+    console.log('a user logged in with the name: ' + socket.request.user.name);
 });
 
 module.exports= socketApi;
