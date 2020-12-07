13494966a69b3d27b90530f2de8ead0e495bd004
Skynessthedark <qksnn.9@gmail.com>
2020-12-07 17:43:59 +0300

    Chat View

--- public/javascripts/controllers/chatController.js
+++ public/javascripts/controllers/chatController.js
@@ -1,3 +1,9 @@
 app.controller('chatController', ['$scope', ($scope)=>{
+    $scope.activeTab = 2;
+
+    $scope.tabChange = tab =>{
+        $scope.activeTab = tab;
+    }
+
     const socket = io.connect("http://localhost:3000");
 }]);
