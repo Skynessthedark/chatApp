e2842605a0713d7a35b9544f086c24460a187dd1
Skynessthedark <qksnn.9@gmail.com>
2020-12-08 17:58:42 +0300

    Rooms

--- public/javascripts/controllers/chatController.js
+++ public/javascripts/controllers/chatController.js
@@ -2,13 +2,18 @@ app.controller('chatController', ['$scope', ($scope)=>{
     $scope.activeTab = 2;
     $scope.onlineList = [];
 
-    $scope.tabChange = tab =>{
-        $scope.activeTab = tab;
-    }
-
     const socket = io.connect("http://localhost:3000");
 
     socket.on('onlineList', users =>{
         $scope.onlineList = users;
     });
+
+    $scope.newRoom = ()=>{
+        let randomName = Math.random().toString(36).substring(7);
+        socket.emit('newRoom', randomName);
+    }
+
+    $scope.tabChange = tab =>{
+        $scope.activeTab = tab;
+    }
 }]);
